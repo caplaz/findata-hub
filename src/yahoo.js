11d8fff27803946c1ef9d99219275b7ed6131693
Stefano Acerbetti <acerbetti@gmail.com>
2025-11-14 22:31:41 -0800

    fix: Suppress yahoo-finance2 survey notice
    - Add suppressNotices: ['yahooSurvey'] to YahooFinance constructor
    - Eliminates the survey message that appears on first API call
    - Cleaner server startup logs

--- src/yahoo.js
+++ src/yahoo.js
@@ -1,4 +1,6 @@
 import YahooFinance from "yahoo-finance2";
 
-const yahooFinance = new YahooFinance();
+const yahooFinance = new YahooFinance({
+  suppressNotices: ['yahooSurvey']
+});
 export default yahooFinance;
